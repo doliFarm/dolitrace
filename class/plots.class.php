5fe9403f9d72489ec51ff12870cbcde7674396bd
grillol <luigi.grillo@gmail.com>
2023-01-08 00:20:31 +0100

    test di push
    just a test

--- class/plots.class.php
+++ class/plots.class.php
@@ -28,7 +28,7 @@ require_once DOL_DOCUMENT_ROOT.'/core/class/commonobject.class.php';
 //require_once DOL_DOCUMENT_ROOT . '/product/class/product.class.php';
 
 /**
- * Class for Plots
+ * Class for Plot
  */
 class Plots extends CommonObject
 {
